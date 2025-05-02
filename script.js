16cbd04e02fe2526f150b0558e4c779bf5d27cb5
n3doter <smirnovbust@gmail.com>
2025-05-02 17:42:17 +0200

    login

--- script.js
+++ script.js
@@ -240,3 +240,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+const container = document.querySelector(".container");
+const registerBtn = document.querySelector(".register-btn");
+const loginBtn = document.querySelector(".login-btn");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
